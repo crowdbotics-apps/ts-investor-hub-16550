040649c8d01b351d19ab313f2ffa07c5f251dd95
Crowdbotics <team@crowdbotics.com>
2020-05-03 02:54:06 +0000

    Initial commit.

---
+++ backend/ts_investor_hub_16550/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ts_investor_hub_16550 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ts_investor_hub_16550.settings')
+
+application = get_wsgi_application()
